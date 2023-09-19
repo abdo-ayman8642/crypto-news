36037998e09658d7c902bc6c7a24d4c93815d0d6
Ahmed Kamal <abdelrahman.ayman8642@gmail.com>
2023-09-19 17:03:04 +0300

    making home page and configure store

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,11 +1,29 @@
 import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
-
+import { useDispatch, useSelector } from "react-redux";
+import { fetchData, useAppDispatch, useAppSelector } from "../store";
 const { Title } = Typography;
 
 function Homepage() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchData());
+  }, [dispatch]);
+  const { data, loading, error } = useAppSelector((state) => state.data);
+  const globalStats = data?.data?.stats;
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <>
       <Title level={2} className="heading">
@@ -13,19 +31,29 @@ function Homepage() {
       </Title>
       <Row>
         <Col span={12}>
-          <Statistic title="Total Cryptocurrencies"></Statistic>
+          <Statistic
+            title="Total Cryptocurrencies"
+            value={millify(globalStats?.total) || 0}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Exchanges"></Statistic>
+          <Statistic
+            title="Total Exchanges"
+            value={millify(globalStats?.totalExchanges) || 0}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Market Cap"></Statistic>
+          <Statistic
+            title="Total Market Cap"
+            value={millify(globalStats?.totalMarketCap) || 0}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total 24h Volume"></Statistic>
+          <Statistic
+            title="Total 24h Volume"
+            value={millify(globalStats?.total24hVolume) || 0}></Statistic>
         </Col>
         <Col span={12}>
-          <Statistic title="Total Markets"></Statistic>
+          <Statistic
+            title="Total Markets"
+            value={millify(globalStats?.totalMarkets) || 0}></Statistic>
         </Col>
       </Row>
     </>
