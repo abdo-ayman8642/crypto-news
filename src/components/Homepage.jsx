6c2c08ca5c870e079cc10b7362107072bcaf87a1
Abdelrahman <abdelrahman.ayman8642@gmail.com>
2023-09-22 03:17:39 +0300

    continue crypto app

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -1,27 +1,18 @@
 import React from "react";
 import millify from "millify";
 import { Typography, Row, Col, Statistic } from "antd";
-import { useEffect } from "react";
 import { Link } from "react-router-dom";
-import { useDispatch, useSelector } from "react-redux";
-import { fetchData, useAppDispatch, useAppSelector } from "../store";
+import { Cryptocurrencies, News } from "./index";
+import { useGetCryptosQuery } from "../store/app/cryptoApi";
+import Loader from "./Loader";
 const { Title } = Typography;
 
 function Homepage() {
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(fetchData());
-  }, [dispatch]);
-  const { data, loading, error } = useAppSelector((state) => state.data);
+  const { data, isFetching } = useGetCryptosQuery(10);
   const globalStats = data?.data?.stats;
 
-  if (loading) {
-    return <div>Loading...</div>;
-  }
-
-  if (error) {
-    return <div>Error: {error}</div>;
+  if (isFetching) {
+    return <Loader />;
   }
 
   return (
@@ -33,29 +24,52 @@ function Homepage() {
         <Col span={12}>
           <Statistic
             title="Total Cryptocurrencies"
-            value={millify(globalStats?.total) || 0}></Statistic>
+            value={millify(globalStats?.total) || 0}
+          ></Statistic>
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Exchanges"
-            value={millify(globalStats?.totalExchanges) || 0}></Statistic>
+            value={millify(globalStats?.totalExchanges) || 0}
+          ></Statistic>
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Market Cap"
-            value={millify(globalStats?.totalMarketCap) || 0}></Statistic>
+            value={millify(globalStats?.totalMarketCap) || 0}
+          ></Statistic>
         </Col>
         <Col span={12}>
           <Statistic
             title="Total 24h Volume"
-            value={millify(globalStats?.total24hVolume) || 0}></Statistic>
+            value={millify(globalStats?.total24hVolume) || 0}
+          ></Statistic>
         </Col>
         <Col span={12}>
           <Statistic
             title="Total Markets"
-            value={millify(globalStats?.totalMarkets) || 0}></Statistic>
+            value={millify(globalStats?.totalMarkets) || 0}
+          ></Statistic>
         </Col>
       </Row>
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Top 10 Cryptos In The World
+        </Title>
+        <Title level={3} className="show-more">
+          <Link to="/cryptocurrencies">Show more</Link>
+        </Title>
+      </div>
+      <Cryptocurrencies simplified preview />
+      <div className="home-heading-container">
+        <Title level={2} className="home-title">
+          Latest Crypto News
+        </Title>
+        <Title level={3}>
+          <Link to="/news">Show more</Link>
+        </Title>
+      </div>
+      <News simplified />
     </>
   );
 }
--- src/store/index.js
+++ src/store/index.js
@@ -1,49 +1,15 @@
-// src/store.js
 import { configureStore } from "@reduxjs/toolkit";
-import { useDispatch, useSelector } from "react-redux";
 
-// Reducer
-const initialState = {
-  data: [],
-  loading: false,
-  error: null,
-};
+import { cryptoApi } from "./app/cryptoApi";
+import { cryptoNewsApi } from "./app/cryptoNewsApi";
 
-const dataReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "FETCH_DATA_REQUEST":
-      return { ...state, loading: true, error: null };
-    case "FETCH_DATA_SUCCESS":
-      return { ...state, loading: false, data: action.payload };
-    case "FETCH_DATA_FAILURE":
-      return { ...state, loading: false, error: action.payload };
-    default:
-      return state;
-  }
-};
-
-// Actions
-export const fetchData = () => async (dispatch) => {
-  dispatch({ type: "FETCH_DATA_REQUEST" });
-
-  try {
-    const response = await fetch("https://api.coinranking.com/v2/coins"); // Replace with your API endpoint
-    const data = await response.json();
-
-    dispatch({ type: "FETCH_DATA_SUCCESS", payload: data });
-  } catch (error) {
-    dispatch({ type: "FETCH_DATA_FAILURE", payload: error.message });
-  }
-};
-
-// Redux store
-const store = configureStore({
+export default configureStore({
   reducer: {
-    data: dataReducer,
+    [cryptoApi.reducerPath]: cryptoApi.reducer,
+    [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware()
+      .concat(cryptoApi.middleware)
+      .concat(cryptoNewsApi.middleware),
 });
-
-export const useAppDispatch = () => useDispatch();
-export const useAppSelector = (selector) => useSelector(selector);
-
-export default store;
